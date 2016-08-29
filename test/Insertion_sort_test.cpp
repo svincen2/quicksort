c0bb40c5b26cf56fe6a76e3ea6418537e7302e1f
Sean Vincent <vincent.sean@hotmail.com>
2016-08-28 22:08:54 -0600

    Fixes insertion sort test to use range params

--- test/Insertion_sort_test.cpp
+++ test/Insertion_sort_test.cpp
@@ -6,7 +6,7 @@ SCENARIO("Insertion sorting a vector of ints") {
 	GIVEN("The vector: (4, 6, -2, 8, 7, -8, 10, 1, 2, 5)") {
 		std::vector<int> vec{4, 6, 2, 8, 7, 8, 10, 1, 2, 5};
 		WHEN("Sorting with insertion sort") {
-			insertion_sort(vec);
+			insertion_sort(vec, 0, vec.size());
 			THEN("Vector is sorted in ascending order") {
 				REQUIRE(vec.size() == 10);
 				for (int i = 0; i < vec.size() - 1; ++i) {
